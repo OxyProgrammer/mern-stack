9f0fe00dc8823edf319c746c9919eafe0033b968
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-17 18:25:07 +0530

    Changed the styles to make it look better, implemeneted the register view

--- FRONTEND/components/Layout.js
+++ FRONTEND/components/Layout.js
@@ -21,29 +21,29 @@ const layout = ({ children }) => {
       <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous" />
+        crossOrigin="anonymous" />
       <link rel="stylesheet" href="/static/css/styles.css" />
     </React.Fragment>
   );
 
   const nav = () => (
-    <ul className="nav nav-tabs bg-warning">
+    <ul className="nav nav-tabs bg-dark">
 
       <li className="nav-item">
         <Link href="/">
-          <a className="nav-link text-dark">Home</a>
+          <a className="nav-link text-light">Home</a>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link href="/login">
-          <a className="nav-link text-dark">Login</a>
+          <a className="nav-link text-light">Login</a>
         </Link>
       </li>
 
       <li className="nav-item">
         <Link href="/register">
-          <a className="nav-link text-dark">Register</a>
+          <a className="nav-link text-light">Register</a>
         </Link>
       </li>
 
@@ -54,5 +54,3 @@ const layout = ({ children }) => {
 }
 
 export default layout;
-
-
--- FRONTEND/pages/register.js
+++ FRONTEND/pages/register.js
@@ -1,5 +1,72 @@
+import { useState } from 'react';
 import Layout from '../components/Layout';
 
-const register = () => <Layout>Register!</Layout>;
+const register = () => {
+
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: '',
+    buttonText: 'Register'
+  });
+
+  const { name, email, password, error, success, buttonText } = state;
+
+  const handleChange = (inputId) => (event) => {
+    setState({ ...state, [inputId]: event.target.value, error: '', success: '', buttonText: 'Register' });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.table(name,email,password);
+  }
+
+  const registerForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+
+        <div className="form-group">
+          <input onChange={handleChange('name')} 
+            value={name}
+            type="text" 
+            className="form-control" 
+            placeholder="Enter your name" />
+        </div>
+
+        <div className="form-group">
+          <input onChange={handleChange('email')} 
+            value={email}
+            type="email" 
+            className="form-control" 
+            placeholder="Enter your email" />
+        </div>
+
+        <div className="form-group">
+          <input onChange={handleChange('password')} 
+            value={password}
+            type="password" 
+            className="form-control" 
+            placeholder="Enter your password" />
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-primary">{buttonText}</button>
+        </div>
+
+      </form>)
+  }
+
+  return (<Layout>
+    <div className="col-md-6 offset-md-3">
+      <h1>Register </h1>
+      <br/>
+      {registerForm()}
+      <br/>
+      {JSON.stringify(state)}
+    </div>
+  </Layout >);
+};
 
 export default register;
