134ed05ed95292840f3599af33b3811703c7e6a7
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-17 16:52:08 +0530

    Added NProgress to show navbar progress. Its working.

--- FRONTEND/components/Layout.js
+++ FRONTEND/components/Layout.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import Head from 'next/head';
+// import Head from 'next/head';
+import Router from 'next/router';
 import Link from 'next/link';
+import NProgress from 'nprogress';
+
+//#region The following code is responsible for the navbar progress bar.
+/*https://nextjs.org/docs/api-reference/next/router*/
+Router.onRouteChangeStart = url => { NProgress.start(); }
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
+
+//#endregion
+
 
 const layout = ({ children }) => {
 
   const head = () => (
-    <link rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-      crossorigin="anonymous" />
+    <React.Fragment>
+      <link rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous" />
+      <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+        integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+        crossorigin="anonymous" />
+    </React.Fragment>
   );
 
   const nav = () => (
