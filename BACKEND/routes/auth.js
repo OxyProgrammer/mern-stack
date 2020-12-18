43a9f4c1533e4899406bbfd119ebd390b8041a45
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-18 16:36:02 +0530

    Started the server code and added the first route.

---
+++ BACKEND/routes/auth.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+
+
+router.get('/register', (req, res) => {
+  res.json({
+    data: 'you hit register end point middleware'
+  })
+});
+
+module.exports=router;
