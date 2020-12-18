51725248e3b379d5c189816cdc65636e184b84ec
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-18 17:06:23 +0530

    Added model and controller

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -1,11 +1,9 @@
 const express=require('express');
 const router=express.Router();
 
+//import from controllers
+const {register}=require('../controllers/auth')
 
-router.get('/register', (req, res) => {
-  res.json({
-    data: 'you hit register end point middleware'
-  })
-});
+router.get('/register', register);
 
 module.exports=router;
