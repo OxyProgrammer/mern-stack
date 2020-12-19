cb9642fa1fc8368114ccd06f569db1b31dba4caf
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-19 18:40:05 +0530

    Changes done to create jwt and include the token in the email.

--- BACKEND/controllers/auth.js
+++ BACKEND/controllers/auth.js
@@ -1,5 +1,7 @@
-
+const User=require('../models/user');
 const AWS=require('aws-sdk');
+const jwt=require('jsonwebtoken');
+const {registerEmailParams} =require('../helpers/email');
 
 AWS.config.update({
   accessKeyId:process.env.AWS_ACCESS_KEY_ID,
@@ -11,38 +13,44 @@ const ses=new AWS.SES({apiVersion:'2010-12-01'});
 
 
 exports.register = (req, res) => {
+
   const{name,email,password}=req.body;
-  const params={
-    Source: process.env.EMAIL_FROM,
-    Destination:{
-      ToAddresses:[email]
-    },
-    ReplyToAddresses:[process.env.EMAIL_TO],
-    Message:{
-      Body:{
-        Html:{
-          Charset:'UTF-8',
-          Data:`<html><body><h1 style='color:red'>Hello ${name}</h1><p>Test email</p></body></html>`
-        }
-      },
-      Subject:{
-        Charset:'UTF-8',
-        Data: 'Complete your registration'
-      }
-    }
-  };
+  //check if user exists in db
+  User.findOne({email}).exec((err,user)=>{
+    if(user){
+      console.log(err);
+      return res.status(400).json({
+        error:'Email is already being used by another guy!'
+      });
+    };
 
-  const sendEmailOnregister=ses.sendEmail(params).promise();
+    //generate json web token with username, email and password
+    let token=jwt.sign({name,email,password},process.env.JWT_ACCOUNT_ACTIVATION,{
+      expiresIn:'30m'
+    });
 
-  sendEmailOnregister
-  .then(data=>{
-    console.log('email submitted to ses!',data);
-    res.send('Email sent');
-  })
-  .catch(error=>{
-    console.log(error);
-    res.send('email failed');
-  });
+    const params=registerEmailParams(email,token);
+
+    const sendEmailOnregister=ses.sendEmail(params).promise();
+
+    sendEmailOnregister
+    .then(data=>{
+      console.log('email submitted to ses!',data);
+      res.json({
+        message:`Email has been sent to ${email}, follow the instuctions on complete your registration.`
+      });
+    })
+    .catch(error=>{
+      console.log(error);
+      res.json({
+        error:`We could not verify your email. Please try again.`
+      })
+    });
+    });
+
+ 
+
+  
 };
 
 
