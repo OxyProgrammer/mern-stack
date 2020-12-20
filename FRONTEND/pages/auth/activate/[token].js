2b184317cdb89ebb54128f4c4673313f7bb424b0
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-20 21:47:33 +0530

    Created the activation page in the UI.

---
+++ FRONTEND/pages/auth/activate/[token].js
@@ -0,0 +1,54 @@
+import {useEffect,useState} from 'react';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import {showSuccessMessage,showErrorMessage} from '../../../helpers/alerts';
+import * as config from '../../../config';
+import {withRouter} from 'next/router';
+import Layout from '../../../components/Layout';
+
+const ActivateAccount=({router})=>{
+
+  const [state,setState]=useState({
+    name:'',
+    token:'',
+    buttonText:'Activate Account',
+    success:'',
+    error:''
+  })
+
+    const {name,token,buttonText,success,error}=state;
+
+useEffect(()=>{
+  let token=router.query.token;
+    if(token){
+      const {name}=jwt.decode(token);
+      setState({...state,name,token});
+    }
+  },[router]);
+
+  const clickSubmit=async event=>{
+    event.preventDefault();
+    setState({...state,buttonText:'Activating'});
+    try{
+      const response=await axios.post(`${config.API}/register/activate`,{token});
+      console.log('response');
+      setState({...state,name:'',token:'',buttonText:'Activate', success:response.data.message});
+    }catch(error){
+      setState({...state,name:'',token:'',buttonText:'Activate Account', error:error.response.data.error});
+    }
+  }
+
+  return <Layout>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h3>G'Day {name}, Ready to activate your account?</h3>
+          <br/>
+          {success && showSuccessMessage(success)}
+          {error && showSuccessMessage(error)}
+          <button className='btn btn-success btn-block' onClick={clickSubmit}>{buttonText}</button>
+        </div>
+    </div>
+  </Layout>
+}
+
+export default withRouter(ActivateAccount);
