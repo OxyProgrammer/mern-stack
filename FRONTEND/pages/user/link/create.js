d53bfb1d5e0df86ed4fe7c51660276e0dca07bf0
Sid <siddhartha.sarkar2705@outlook.com>
2020-12-28 19:59:08 +0530

    Did the panel for entering the title and link.

--- FRONTEND/pages/user/link/create.js
+++ FRONTEND/pages/user/link/create.js
@@ -2,7 +2,7 @@ import {useEffect,useState} from 'react';
 import axios from 'axios';
 import * as config from '../../../config';
 import Layout from '../../../components/Layout';
-import {showSuccessMessage,showErrorMessage} from ',,/../../helpers/alerts';
+import {showSuccessMessage,showErrorMessage} from '../../../helpers/alerts';
 
 const create=()=>{
 
@@ -18,6 +18,36 @@ const create=()=>{
   });
 
   const {title,url,categories,loadedCategories,success,error,medium,type}=state;
+
+  const handleTitleChange = event=>{
+    setState({...state, title:event.target.value,error:'',success:''});
+  }
+  
+  const handleURLChange = event=>{
+    setState({...state, url:event.target.value,error:'',success:''});
+  }
+
+  const handleSubmit = async event=>{
+    
+  }
+
+  const submitLinkFrom=()=>{
+    return (<form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="text-muted">Title</label>
+        <input type="text" className="form-control" onChange={handleTitleChange} value={title}></input>
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">URL</label>
+        <input type="text" className="form-control" onChange={handleURLChange} value={url}></input>
+      </div>
+
+      <div>
+        <button className="btn btn-success float-right" type="submit">Submit</button>
+      </div>
+    </form>)
+  }
 
   const loadCategories = async ()=>{
     try{
@@ -39,7 +69,10 @@ const create=()=>{
         <div className="col-md-12">
           <h3>Submit Link/URL</h3>
           <br/>
-          {JSON.stringify(loadedCategories.length)}
+          <div className="row">
+            <div className="col-md-4">XXXX</div>
+            <div className="col-md-8">{submitLinkFrom()}</div>
+          </div>
         </div>
       </div>
     </Layout>);
